7716e58117aeea5b982d44a8cbf3d255289a37e0
Maxym Galenko <89601711+MaxGalenko@users.noreply.github.com>
2024-05-11 18:58:10 -0400

    Now only extracts the article without ads

--- WebToPlainText.py
+++ WebToPlainText.py
@@ -5,15 +5,29 @@ url = "https://gamerant.com/epic-games-store-free-games-march-2024-deus-ex-bridg
 html = urlopen(url).read()
 soup = BeautifulSoup(html, features="html.parser")
 
-# kill all script and style elements
-for script in soup(["script", "style"]):
-    script.extract()    # rip it out
+# Find the element with id "article-body"
+article_body = soup.find(id="article-body")
 
-# get text
-plain_text = soup.get_text()
+# Extract text only from the article body
+if article_body:
+    # Remove script and style elements from the article body
+    for script in article_body(["script", "style"]):
+        script.extract()    # rip it out
 
-# split text into an array
-plain_text_split = plain_text.split()
+    # Remove all div elements from the article body except those with class "content-block-regular"
+    for div in article_body.find_all("div"):
+        if "content-block-regular" not in div.get("class", []):
+            div.extract()
 
-# write text in one line
-cleaned_plain_text = " ".join(plain_text_split)
+    # Get text from the article body
+    article_text = article_body.get_text()
+
+    # Split text into an array
+    article_text_split = article_text.split()
+
+    # Write text in one line
+    cleaned_article_text = " ".join(article_text_split)
+
+    print(cleaned_article_text)
+else:
+    print("Could not find article body with id 'article-body'")
